50b1fcd4c95d55d446694a61e5c6d6098e48cda1
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-03-25 17:36:27 -0700

    basic logic for box component

--- src/box.js
+++ src/box.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 import './box.css'
 
 class Box extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            boxStatus: true,
+            boxColor: {backgroundColor: 'rgb(87 ,87 ,87)'}
+        };
+        this.handleClickBox = this.handleClickBox.bind(this);
+    }
+
+    handleClickBox(e){
+        console.log('work')
+        if (this.state.boxStatus === true){
+            return this.setState({ boxStatus : false, boxColor: {backgroundColor: 'rgb(36, 253, 253)'}})
+        } else if (this.state.boxStatus === false){
+            return this.setState({ boxStatus: true, boxColor: {backgroundColor: 'rgb(87 ,87 ,87)'}})
+        }
+    }
     render(){
         return(
-            <div className={`boxStyle ${this.props.lit == true && `boxLit`}`} ></div>
+            
+            
+            <div onClick={this.handleClickBox} style= {this.state.boxColor} className={`boxStyle`} ></div>
+            
             
         )
     }
--- src/grid.js
+++ src/grid.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react';
 import Box from './box.js'
+import './grid.css'
 
 class Grid extends Component {
+
+    static defaultProps ={
+        title: '5x5 Lights Out',
+        row: 5,
+        column: 5
+    }
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        };
+    }
+   
+   
+   
     render(){
         return(
-            <div>
-            <Box lit={true} />
-            <Box lit={false} />
+            <div className='container mt-5'>
+            <div className='row'>
+            <Box  />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            </div>
+            <div className='row'>
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            </div>
+            <div className='row'>
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            </div>
+            <div className='row'>
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            </div>
+            <div className='row'>
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            </div>
             </div>
         )
     }
