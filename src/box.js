c2be228e965c6b590375ce4edd245d74942c5c6c
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-03-25 21:30:27 -0700

    grid logic progress

--- src/box.js
+++ src/box.js
@@ -2,29 +2,10 @@ import React, { Component } from 'react';
 import './box.css'
 
 class Box extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            boxStatus: true,
-            boxColor: {backgroundColor: 'rgb(87 ,87 ,87)'}
-        };
-        this.handleClickBox = this.handleClickBox.bind(this);
-    }
-
-    handleClickBox(e){
-        console.log('work')
-        if (this.state.boxStatus === true){
-            return this.setState({ boxStatus : false, boxColor: {backgroundColor: 'rgb(36, 253, 253)'}})
-        } else if (this.state.boxStatus === false){
-            return this.setState({ boxStatus: true, boxColor: {backgroundColor: 'rgb(87 ,87 ,87)'}})
-        }
-    }
     render(){
         return(
             
-            
-            <div onClick={this.handleClickBox} style= {this.state.boxColor} className={`boxStyle`} ></div>
-            
+            <div className={`boxStyle ${this.props.lit == true && `boxLit`}`} ></div>
             
         )
     }
--- src/grid.js
+++ src/grid.js
@@ -7,56 +7,60 @@ class Grid extends Component {
     static defaultProps ={
         title: '5x5 Lights Out',
         row: 5,
-        column: 5
+        column: 5,
+        chance: 0.25
     }
     constructor(props){
         super(props);
         this.state = {
-            
+        win: false,
+        board: this.createBoard()
         };
+        this.handleClick= this.handleClick.bind(this);
     }
+
+
+    createBoard(){
+    let board = []
+    for(let y=0;y<this.props.row;y++){
+        let row =[];
+       for (let x=0; x<this.props.column; x++){
+         row.push(Math.random() < this.props.chance)
+       }
+        board.push(row);
+    }
+    return board
+    }
+
+   handleClick(){
+       if(this.state.lit){
+           return this.setState({lit: false})
+       } else if (!this.state.lit){
+           return this.setState({lit: true})
+       }
+   }
    
-   
+  
    
     render(){
+    
+
         return(
-            <div className='container mt-5'>
-            <div className='row'>
-            <Box  />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            </div>
-            <div className='row'>
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            </div>
-            <div className='row'>
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            </div>
-            <div className='row'>
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            </div>
-            <div className='row'>
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            </div>
-            </div>
+           <div>
+               <h1>{this.props.title}</h1>
+               <h1>{this.props.board}</h1>
+           <table>
+            
+            <tr>
+            <th><Box /></th>
+            <th><Box /></th>
+            <th><Box /></th>   
+            <th><Box /></th>        
+            <th><Box /></th>        
+            </tr>
+            
+         </table>
+         </div>
         )
     }
 }
